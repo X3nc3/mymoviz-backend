de9efa00b439310f041239333708a3835e5d8d18
Maxence <maxence.ogier@gmail.com>
2023-11-15 14:40:05 +0100

    first commit

---
+++ routes/index.js
@@ -0,0 +1,22 @@
+var express = require("express");
+var router = express.Router();
+
+const APIKEY = process.env.KEY_API;
+
+console.log(APIKEY);
+
+router.get("/movie", (req, res) => {
+  fetch(
+    `https://api.themoviedb.org/3/movie/now_playing?api_key=${APIKEY}&language=fr-FR`
+  )
+    .then((response) => response.json())
+    .then((findMovies) => {
+      if (findMovies.results.length > 0) {
+        res.json({ findMovies });
+      } else {
+        res.json({ error: "Not movies find!" });
+      }
+    });
+});
+
+module.exports = router;
